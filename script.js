741990d43dbfe615cb8c563c7cca6bc995aa2c48
cd ~/code/$GITHUB_USERNAME/dotfiles && zsh git_setup.sh <jankee112@freenet.de>
2022-02-16 21:52:50 +0100

    scriptjs / manifest.js change logic

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
+
+
+const tagsToIgnore = ['SCRIPT', 'NOSCRIPT', 'STYLE', 'SOURCE']
 replaceText(document.body)
-// const tagsToIgnore = ['SCRIPT', 'NOSCRIPT', 'STYLE', 'SOURCE']
 
 function replaceText(element) {
     if (element.hasChildNodes()) {
         element.childNodes.forEach(replaceText)
     } else if (element.nodeType === Text.TEXT_NODE) {
-        element.textContent = element.textContent.replace(/coronavirus/gi, "")
+        if (element.textContent.match(/coronavirus/gi)) {
+            const newElement = document.createElement('span')
+            newElement.innerHTML = element.textContent.replace(/(coronavirus)/gi, '<span class="rainbow">$1</span>')
+            element.replaceWith(newElement)
+        }
     }
 }
