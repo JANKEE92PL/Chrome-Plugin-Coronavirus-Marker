a6e2007012f5058e4da4ad2f90cfca75b0a4d791
cd ~/code/$GITHUB_USERNAME/dotfiles && zsh git_setup.sh <jankee112@freenet.de>
2022-02-16 21:49:09 +0100

    add pics

--- script.js
+++ script.js
@@ -1,13 +1,10 @@
 replaceText(document.body)
+// const tagsToIgnore = ['SCRIPT', 'NOSCRIPT', 'STYLE', 'SOURCE']
 
 function replaceText(element) {
     if (element.hasChildNodes()) {
         element.childNodes.forEach(replaceText)
     } else if (element.nodeType === Text.TEXT_NODE) {
-        if (element.textContent.match(/coronavirus/gi)) {
-            const newElement = document.createElement('span')
-            newElement.innerHTML = element.textContent.replace(/(coronavirus)/gi, '<span class="rainbow highlight">$1</span>')
-            element.replaceWith(newElement)
-        }
+        element.textContent = element.textContent.replace(/coronavirus/gi, "")
     }
 }
